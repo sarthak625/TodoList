b39dfda5ba8c9b6fef1a5b2a8054e0e80d3c9f58
Sarthak <sarthakn18@gmail.com>
2017-06-15 15:53:12 +0530

    Added icons and span animations

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,7 +19,7 @@ $("input[type='text']").keypress(function(event){
 			$(this).val("");
 
 			//create a new li and add to ul
-			$('ul').append('<li><span>X</span> '+todoText+'</li>')
+			$('ul').append('<li><span><i class="fa fa-trash"></i></span>'+todoText+'></li>')
 		}
 	}
 })
